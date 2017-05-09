f35c1b603501aca949e7cfa0396f7bb52b5dd879
zinshiti <zinshiti@gmail.com>
2017-05-09 19:20:00 +0900

    Update callback.php

--- web/callback.php
+++ web/callback.php
@@ -132,7 +132,7 @@ if ($text == 'はい') {
 } else {
   $response_format_text = [
     "type" => "template",
-    "altText" => "こんにちわ 何かご用ですか？（はい／いいえ）",
+    "altText" => "hello 何かご用ですか？（はい／いいえ）",
     "template" => [
         "type" => "confirm",
         "text" => "こんにちわ 何かご用ですか？",
